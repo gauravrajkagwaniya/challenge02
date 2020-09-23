442f7c8091719c63d278540558b505755851720f
Gaurav Raj kagwaniya <gauravrajkagwaniya@gmail.com>
2020-09-24 01:53:09 +0530

    splash page and few fix with drawer

--- android/app/src/main/kotlin/com/grk/page_ui/MainActivity.kt
+++ android/app/src/main/kotlin/com/grk/page_ui/MainActivity.kt
@@ -2,5 +2,8 @@ package com.grk.page_ui
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
